ad374d944fd57639f9df93e1d05fae194bb15aa9
Andrija Sagicc <andrija.sagic@gmail.com>
2022-04-30 23:45:33 +0200

    Resolve unmount from DOM
    This PR resolves issue for MUI duplication, unmount from DOM.

--- pages/default.vue
+++ pages/default.vue
@@ -6,9 +6,10 @@ import Mirador from 'mirador/dist/es/src/index'
 import { miradorImageToolsPlugin } from 'mirador-image-tools'
 
 export default {
-  components: {
-  },
-  mounted() {
+  
+  computed: {
+    miradorViewerOptions() {
+    
     const manifest = "https://iiif.bodleian.ox.ac.uk/iiif/manifest/e32a277e-91e2-4a6d-8ba6-cc4bad230410.json"
     const config = {
       id: "demo",
@@ -18,7 +19,16 @@ export default {
         },
       ],
     }
-    Mirador.viewer(config, [...miradorImageToolsPlugin])
+    return config;
   },
+  
+  mounted() {
+    this.mirador = Mirador.viewer(this.miradorViewerOptions, [...miradorImageToolsPlugin]);
+  },
+
+  beforeDestroy() {
+    this.mirador.unmount()
+  }
+  
 }
 </script>
